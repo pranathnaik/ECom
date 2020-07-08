18ed640a7164f5c1d993f2f87e2cffd0bfbe1cd0
Pranath Naik <pranathnaik12@gmail.com>
2020-07-08 19:08:41 +0530

    first Commit

---
+++ services/src/components/comp.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>hello world</h1>
+        <button>increament</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
