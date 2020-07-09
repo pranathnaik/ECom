3d6606c27b1db96653443d430a02cee39acdd490
Pranath Naik <pranathnaik12@gmail.com>
2020-07-09 11:16:52 +0530

    new files

--- services/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/comp';
-
-
-ReactDOM.render(<Counter/>,document.getElementById('root'));
