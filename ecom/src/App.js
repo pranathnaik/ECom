974b1157294b4710b6dbecb2885db709af5afcfb
Pranath Naik <pranathnaik12@gmail.com>
2020-07-09 12:34:06 +0530

    React Front commit

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from './data';
+import{BrowserRouter,Route} from 'react-router-dom';
+import "./App.css";
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  };
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <button onClick={openMenu}>&#9776;</button>
+          <a href="index.html">FLIPKARTO</a>
+        </div>
+        <div className="header-links">
+          <a href="cart.html">CART</a>
+          <a href="signin">SIGN IN</a>
+        </div>
       </header>
+
+      <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>
+          x
+        </button>
+        <ul>
+          <li>
+            <a href="index.html">Pants</a>
+          </li>
+          <li>
+            <a href="index.html">Shirts</a>
+          </li>
+        </ul>
+      </aside>
+
+      <main className="main">
+        <div className="content">
+          <Route path="/products/:id" component={ProductScreen}/>
+          <Route path="/" exact={true} component={HomeScreen}/>
+          <ul className="products">
+            {
+              data.products.map(product =>    
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src={product.image}
+                  alt="product"
+                />
+                <div className="product-name">
+                  <a href="product.html">{product.name}</a>
+                </div>
+                <div className="product-brand">{product.brand}</div>
+                <div className="product-price">{product.price}</div>
+                <div className="product-rating">{product.rating}</div>
+              </div>
+            </li>)
+              }     
+          </ul>
+        </div>
+      </main>
+
+      <footer className="footer">All right reserverd</footer>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ ecom/src/data.js
@@ -0,0 +1,43 @@
+export default {
+  products: [
+    {
+      name: "Slim shirt",
+      category: "Shirts",
+      image: "/images/d1.jpg",
+      price: 20,
+      brand: "Nike",
+      rating: 4.2,
+      numReviews: 9,
+    },
+
+    {
+      name: "Fit shirt",
+      category: "Shirts",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 3.5,
+      numReviews: 10,
+    },
+
+    {
+      name: "Slim pants",
+      category: "Pants",
+      image: "/images/d1.jpg",
+      price: 30,
+      brand: "Nike",
+      rating: 2.5,
+      numReviews: 7,
+    },
+
+    {
+        name: "Slim pants",
+        category: "Pants",
+        image: "/images/d1.jpg",
+        price: 30,
+        brand: "Nike",
+        rating: 2.5,
+        numReviews: 7,
+      },
+  ],
+};
